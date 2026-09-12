ada640a813b9f6242bcf8772ad81c4d99188d869
Qin Yang <qin.yang@alumnos.upm.es>
2026-09-12 12:42:30 +0200

    Add DELETE user by id tests#12

---
+++ src/test/java/es/upm/miw/devops/code/services/UserServiceTest.java
@@ -0,0 +1,34 @@
+package es.upm.miw.devops.code.services;
+
+import es.upm.miw.devops.code.User;
+import es.upm.miw.devops.code.UsersDatabase;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class UserServiceTest {
+
+    private UserService userService;
+
+    @BeforeEach
+    void setUp() {
+        UsersDatabase usersDatabase = new UsersDatabase();
+        userService = new UserService(usersDatabase);
+    }
+
+    @Test
+    void testDeleteById() {
+        boolean deleted = userService.deleteById("1");
+
+        assertThat(deleted).isTrue();
+        assertThat(userService.findById("1")).isEmpty();
+    }
+
+    @Test
+    void testDeleteByIdNotFound() {
+        boolean deleted = userService.deleteById("999");
+
+        assertThat(deleted).isFalse();
+    }
+}
